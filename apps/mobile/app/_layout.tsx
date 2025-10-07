b281369a02d4154d4a7f56854fca1a1cb69f19a0
arelquack <arel.zandra@gmail.com>
2025-10-08 00:17:22 +0700

    feat(ui): enhance login and registration with app logo and password toggle

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -4,11 +4,11 @@ import React from 'react';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="register" options={{ title: 'Registrasi' }} />
-      <Stack.Screen name="login" options={{ title: 'Login' }} />
-      <Stack.Screen name="dashboard" options={{ title: 'Dashboard Pasien' }} />
-      <Stack.Screen name="consent" options={{ title: 'Manajemen Izin' }} />
-      <Stack.Screen name="logaccess" options={{ title: 'Log Akses' }} />
+      <Stack.Screen name="register" options={{ title: 'Registrasi', headerShown: false }} />
+      <Stack.Screen name="login" options={{ title: 'Login', headerShown: false }} />
+      <Stack.Screen name="dashboard" options={{ title: 'Dashboard Pasien', headerShown: false }} />
+      <Stack.Screen name="consent" options={{ title: 'Manajemen Izin', headerShown: false }} />
+      <Stack.Screen name="logaccess" options={{ title: 'Log Akses', headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
